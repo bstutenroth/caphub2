86b98f568fa7e8399ad776273387b8cf25c7aefa
Albert Felix <albertrfelix@alberts-air.attlocal.net>
2019-05-30 08:18:19 -0700

    Added HTML and JS files

--- src/main/java/com/google/codeu/data/Datastore.java
+++ src/main/java/com/google/codeu/data/Datastore.java
@@ -25,9 +25,9 @@ import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.google.appengine.api.datastore.Query.SortDirection;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
-import java.util.Set;
 import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
 
 
 /** Provides access to the data stored in Datastore. */
@@ -87,7 +87,7 @@ public class Datastore {
     Set<String> users = new HashSet<>();
     Query query = new Query("Message");
     PreparedQuery results = datastore.prepare(query);
-    for(Entity entity : results.asIterable()) {
+    for (Entity entity : results.asIterable()) {
       users.add((String) entity.getProperty("user"));
     }
     return users;
