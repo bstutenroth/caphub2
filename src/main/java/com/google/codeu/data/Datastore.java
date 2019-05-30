ed0ee03a453bd891f0f4c19f0d7fa4cf06422c78
Bree Betts <breelynbetts@gmail.com>
2019-05-29 19:26:33 -0700

    Added Datastore logic to Datastore.java file

--- src/main/java/com/google/codeu/data/Datastore.java
+++ src/main/java/com/google/codeu/data/Datastore.java
@@ -61,22 +61,40 @@ public class Datastore {
             .addSort("timestamp", SortDirection.DESCENDING);
     PreparedQuery results = datastore.prepare(query);
 
-    for (Entity entity : results.asIterable()) {
+    createMessage(results, messages);
+
+    return messages;
+  }
+
+  public List<Message> getAllMessages() {
+    List<Message> messages = new ArrayList<>();
+
+    Query query = new Query("Message")
+      .addSort("timestamp",SortDirection.DESCENDING);
+    PreparedQuery results = datastore.prepare(query);
+
+    createMessage(results, messages);
+
+    return messages;
+  }
+
+  private void createMessage(PreparedQuery pq, List<Message> m) {
+
+    for (Entity entity : pq.asIterable()) {
       try {
         String idString = entity.getKey().getName();
         UUID id = UUID.fromString(idString);
+        String user = (String) entity.getProperty("user");
         String text = (String) entity.getProperty("text");
         long timestamp = (long) entity.getProperty("timestamp");
 
         Message message = new Message(id, user, text, timestamp);
-        messages.add(message);
+        m.add(message);
       } catch (Exception e) {
         System.err.println("Error reading message.");
         System.err.println(entity.toString());
         e.printStackTrace();
       }
     }
-
-    return messages;
   }
 }
