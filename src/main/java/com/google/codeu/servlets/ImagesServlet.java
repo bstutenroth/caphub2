52a398ea2b8219cbba53f5398f0f9013e8d31868
Albert Felix <albertrfelix@alberts-air.home>
2019-06-08 03:13:50 -0400

    Review - Server not running

---
+++ src/main/java/com/google/codeu/servlets/ImagesServlet.java
@@ -0,0 +1,23 @@
+// package com.google.codeu.servlets;
+//
+// import com.google.codeu.data.Datastore;
+// import com.google.gson.Gson;
+// import java.io.IOException;
+//
+// import java.util.Set;
+// import javax.servlet.annotation.WebServlet;
+// import javax.servlet.http.HttpServlet;
+// import javax.servlet.http.HttpServletRequest;
+// import javax.servlet.http.HttpServletResponse;
+//
+// /**
+//  * Handles fetching all users for the community page.
+//  */
+//
+//  @WebServlet("/user-list")
+//  public class ImagesServlet extends HttpServlet {
+//
+//
+//
+//
+// }
