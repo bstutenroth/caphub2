25f6cd96aaf0e24b65fdf27f8177003f972447c9
Bree Betts <breelynbetts@gmail.com>
2019-06-13 22:06:33 -0500

    Added a csv file to WEB-INF folder and created a servlet that creates json arrays out of the data

---
+++ src/main/java/com/google/codeu/servlets/MapCitiesDataServlet.java
@@ -0,0 +1,55 @@
+package com.google.codeu.servlets;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Returns City data as a JSON array
+ * example: [{"lat": 34.567, "lng": -123.45}]
+ */
+@WebServlet("/map-data")
+public class MapCitiesDataServlet extends HttpServlet {
+
+  private JsonArray popCityArray;
+
+  @Override
+  public void init() {
+    popCityArray = new JsonArray();
+    Gson gson = new Gson();
+    Scanner scanner = new Scanner(getServletContext().getResourceAsStream("/WEB-INF/popularCities.csv"));
+    while (scanner.hasNextLine()) {
+      String line = scanner.nextLine();
+      String[] cells = line.split(",");
+
+      double lat = Double.parseDouble(cells[0]);
+      double lng = Double.parseDouble(cells[1]);
+
+      popCityArray.add(gson.toJsonTree(new MapCity(lat, lng)));
+    }
+    scanner.close();
+  }
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws IOException {
+    response.setContentType("application/json");
+    response.getOutputStream().println(popCityArray.toString());
+  }
+
+  private static class MapCity {
+    double lat;
+    double lng;
+
+    private MapCity(double lat, double lng) {
+      this.lat = lat;
+      this.lng = lng;
+    }
+  }
+}
