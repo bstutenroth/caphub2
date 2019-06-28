c0571c145d0dba18a5fbb18244f9892a5d0f8294
Bree Betts <breelynbetts@gmail.com>
2019-06-28 14:46:45 -0700

    Modified Places API autocomplete feature to allow users to select a location

--- src/main/webapp/js/map.js
+++ src/main/webapp/js/map.js
@@ -1,5 +1,16 @@
 let map;
 let editMarker;
+let placeSearch;
+let autocomplete;
+
+let componentForm = {
+  street_number: 'short_name',
+  route: 'long_name',
+  locality: 'long_name',
+  administrative_area_level_1: 'short_name',
+  country: 'long_name',
+  postal_code: 'short_name'
+};
 
 function createMap() {
   map = new google.maps.Map(document.getElementById('map'), {
@@ -80,4 +91,33 @@ function buildInfoWindowInput(lat, lng) {
   containerDiv.appendChild(document.createElement('br'));
   containerDiv.appendChild(button);
   return containerDiv;
+}
+
+function initAutocomplete() {
+  // Create the autocomplete object, restricting the search predictions to
+  // geographical location types.
+  autocomplete = new google.maps.places.Autocomplete(
+      document.getElementById('autocomplete'), {types: ['geocode']});
+
+  // Avoid paying for data that you don't need by restricting the set of
+  // place fields that are returned to just the address components.
+  autocomplete.setFields(['address_component']);
+
+  // When the user selects an address from the drop-down, populate the
+  // address fields in the form.
+  //autocomplete.addListener('place_changed', fillInAddress);
+}
+
+function geolocate() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      let geolocation = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      let circle = new google.maps.Circle(
+          {center: geolocation, radius: position.coords.accuracy});
+      autocomplete.setBounds(circle.getBounds());
+    });
+  }
 }
