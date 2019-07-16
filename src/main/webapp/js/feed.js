335bbea57394e4c3ccc897d3066713842ad30ca5
Bree Betts <breelynbetts@gmail.com>
2019-07-15 20:14:56 -0700

    Moved upload of images to feed.html page

--- src/main/webapp/js/feed.js
+++ src/main/webapp/js/feed.js
@@ -43,7 +43,63 @@ function buildMessageDiv(message) {
   return messageDiv;
 }
 
+/** Fetches messages and add them to the page. */
+function fetchImages() {
+  const url = '/my-form-handler';
+  fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((images) => {
+        const imagesContainer = document.getElementById('images-container');
+        if (images.length == 0) {
+          imagesContainer.innerHTML = '<p>This user has no posts yet.</p>';
+        } else {
+          imagesContainer.innerHTML = '';
+        }
+        images.forEach((image) => {
+          const imageDiv = buildImageDiv(image);
+          imagesContainer.appendChild(imageDiv);
+        });
+      });
+}
+
+/**
+ * Builds an element that displays the message.
+ * @param {ImageUrl} image
+ * @return {Element}
+ */
+function buildImageDiv(image) {
+  const headerDiv = document.createElement('div');
+  headerDiv.classList.add('message-header');
+  headerDiv.appendChild(document.createTextNode(
+      image.user + ' - ' + new Date(image.timestamp)));
+
+  const bodyDiv = document.createElement('div');
+  bodyDiv.classList.add('message-body');
+  bodyDiv.innerHTML = image.text;
+
+  const locationDiv = document.createElement('div');
+  locationDiv.classList.add('location-body');
+  locationDiv.innerHTML = image.location;
+
+  const imageDiv = document.createElement("div");
+  imageDiv.classList.add('image-body');
+  imageDiv.innerHTML = "<img src=\"" + image.url + "\" />";
+
+  const messageDiv = document.createElement('div');
+  messageDiv.classList.add('message-div');
+  messageDiv.appendChild(headerDiv);
+  messageDiv.appendChild(bodyDiv);
+  messageDiv.appendChild(locationDiv);
+  messageDiv.appendChild(imageDiv);
+
+  return messageDiv;
+}
+
+
 // Fetch data and populate the UI of the page.
 function buildUI() {
   fetchMessages();
+  fetchImages();
 }
